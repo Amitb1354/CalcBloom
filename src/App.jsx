e9a703e4b8f741da8a9d29b83fc78a2aee312381
Amib1354 <87922809+Amib1354@users.noreply.github.com>
2026-06-11 10:34:38 +0545

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Navbar   from './components/Navbar'
 import Footer   from './components/Footer'
 import Home     from './pages/Home'
@@ -25,6 +26,7 @@ export default function App() {
         </main>
         <Footer />
       </div>
+      <Analytics />
     </BrowserRouter>
   )
 }
